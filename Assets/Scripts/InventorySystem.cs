666f5ad8748ebd51d6683c7c646ed42b95fdb045
SriMarn <srirammarni369@gmail.com>
2025-07-16 22:04:16 -0400

    Update InventorySystem.cs

--- Assets/Scripts/InventorySystem.cs
+++ Assets/Scripts/InventorySystem.cs
@@ -1,11 +1,15 @@
+using System.Collections;
 using UnityEngine;
 
 public class InventorySystem : MonoBehaviour
 {
+    public static int inventoryCount = 0;
+    private int weightLoad;
+    private ArrayList itemsHeld;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        weightLoad = 0;
     }
 
     // Update is called once per frame
