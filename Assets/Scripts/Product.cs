76ba8f4cba949faae7273d9eb103cda76bb655ac
StarRevamped <parkey413@gmail.com>
2025-07-18 20:33:22 -0400

    Updated shelf spawning script
    im goated and nobody look at my code.

--- Assets/Scripts/Product.cs
+++ Assets/Scripts/Product.cs
@@ -3,9 +3,35 @@ using UnityEngine;
 
 public class Product : MonoBehaviour
 {
+    public GameObject item;
     public String productName;
     public String container;
-    public bool isStorable;
+    public bool storable;
     public int price;
     public int weight;
+
+    public String GetProductName()
+    {
+        return productName;
+    }
+    public String GetContainer()
+    {
+        return container;
+    }
+    public bool IsStorable()
+    {
+        return storable;
+    }
+    public int GetPrice()
+    {
+        return price;
+    }
+    public int GetWeight()
+    {
+        return weight;
+    }
+    public GameObject GetObject()
+    {
+        return item;
+    }
 }
--- Assets/Scripts/RandomShelfSpawn.cs
+++ Assets/Scripts/RandomShelfSpawn.cs
@@ -1,24 +1,71 @@
+/*
+Script for spawning items on all shelves/containers.
+
+DO NOT PUT THIS SCRIPT ON ANY SHELF OR REPEATED OBJECT. THIS IS TO BE PUT ON A SINGLE ITEM IN THE WHOLE MAP.
+
+*/
 using UnityEngine;
 using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 public class RandomShelfSpawn : MonoBehaviour
 {
+    //rnd is the random number generator. I put it here to stop it from remaking it 3 million times
+    System.Random rnd = new();
+    public String[] spawnerTags;
     private GameObject[] spawnableObjects;
-    public GameObject[] spawningLocations;
+    private List<List<Product>> allProducts = new List<List<Product>>();
+    
+    //List order: shelf,fridge,cashierShelf
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        System.Random rnd = new();
-        spawnableObjects = GameObject.FindGameObjectsWithTag("StorableObject");
-        foreach (GameObject location in spawningLocations)
+        //do NOT judge me. We can for loop this when we know how many containers we have. for now just do it the funky way
+        allProducts.Add(new List<Product>()); //Shelf
+        allProducts.Add(new List<Product>()); //Fridge
+        allProducts.Add(new List<Product>()); //Cashier Shelf
+
+        spawnableObjects = GameObject.FindGameObjectsWithTag("sellingProduct");
+        foreach (GameObject item in spawnableObjects)
         {
-            int randomNumber = rnd.Next(0, spawnableObjects.Length);
-            Instantiate(spawnableObjects[randomNumber], location.transform.position, location.transform.rotation);
+            Product temp = item.GetComponent<Product>();
+            switch (temp.GetContainer())
+            {
+                case "shelf":
+                    allProducts[0].Add(temp);
+                    break;
+                case "fridge":
+                    allProducts[2].Add(temp);
+                    break;
+                case "cashierShelf":
+                    allProducts[1].Add(temp);
+                    break;
+                default:
+                    UnityEngine.Debug.Log("Error in RandomShelfSpawn.cs | Could not find container for " + temp.GetProductName());
+                    break;
+            }
         }
+        //stop reading my code
+        for (var i = 0; i < spawnerTags.Length; i++)
+        {
+            SpawnItems(spawnerTags[i], allProducts[i]);
+        }
+        
     }
     // Update is called once per frame
     void Update()
     {
 
+    }
+    private void SpawnItems(String container, List<Product> productList)
+    {
+        GameObject[] spawnLocations = GameObject.FindGameObjectsWithTag(container);
+        foreach (GameObject location in spawnLocations) {
+            int randomNumber = rnd.Next(0, productList.Capacity);
+            Instantiate(productList[randomNumber].GetObject(), location.transform.position, location.transform.rotation);
+        }
     }
 }
